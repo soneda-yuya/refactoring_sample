33d6695d94065fbaacc17ccf5d94b527c1753ccc
soneda-yuya <soneda.yuya@gmail.com>
2020-09-25 08:32:35 +0900

    外部キー制約をつける

---
+++ db/migrate/20200924232703_add_reference_on_table.rb
@@ -0,0 +1,12 @@
+class AddReferenceOnTable < ActiveRecord::Migration[6.0]
+  def change
+    unless foreign_key_exists?(:category_setting_items, :categories)
+      add_foreign_key :category_setting_items, :categories
+    end
+    add_foreign_key :products, :categories unless foreign_key_exists?(:products, :categories)
+    add_foreign_key :product_category_settings, :products unless foreign_key_exists?(:products, :products)
+    return if foreign_key_exists?(:product_category_settings, :category_setting_items)
+
+    add_foreign_key :product_category_settings, :category_setting_items
+  end
+end
