946dc26167c9c93ce9ee4efc0c0f77c8a04eb284
soneda-yuya <soneda.yuya@gmail.com>
2020-09-25 07:43:00 +0900

    未使用の関数の削除

--- app/models/category_setting_item.rb
+++ app/models/category_setting_item.rb
@@ -3,10 +3,4 @@
 class CategorySettingItem < ApplicationRecord
   belongs_to :category
   has_many :product_category_settings
-
-  class << self
-    def fetch_category_setting_data(product_id:, category_setting_item_id:)
-      ProductCategorySetting.find_by(product_id: product_id, category_setting_item_id: category_setting_item_id)
-    end
-  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,4 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :product_category_settings
-
-  def fetch_category(category_id:)
-    Category.where(category_id: category_id)
-  end
 end
